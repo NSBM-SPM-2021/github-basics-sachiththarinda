677b70bf675f58226f8ef5d8672cdf5f953d7c44
sachiththarinda <sachith.tharinda@gmail.com>
2021-08-18 20:17:08 +0530

    front end

---
+++ client/src/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  
+    heading: {
+      color: 'rgba(0,183,255, 1)',
+    },
+    image: {
+      
+    },
+   
+  }));
