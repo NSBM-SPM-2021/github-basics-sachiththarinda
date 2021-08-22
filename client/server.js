5e0cf9ffa59e66b2bd538153004687e80588894b
sachiththarinda <sachith.tharinda@gmail.com>
2021-08-22 20:04:50 +0530

    routes

--- client/server.js
+++ client/server.js
@@ -20,16 +20,6 @@ app.use('/css', express.static(path.resolve(__dirname, "assests/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assests/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assests/js")))
 
-app.get('/', (req, res) => {
-   res.render('index');
-})
+app.use('/', require('./server/routes/router'))
 
-
-app.get('/add-user', (req, res) => {
-   res.render('add_user');
-})
-
-app.get('/update-patient', (req, res) => {
-   res.render('update_user');
-})
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
---
+++ client/server/services/render.js
@@ -0,0 +1,13 @@
+
+
+exports.homeRoutes =(req,res)=>{
+    res.render('index');
+}
+
+exports.add_patient =(req,res)=>{
+    res.render('add_patient');
+}
+
+exports.update_patient =(req,res)=>{
+    res.render('update_patient');
+}
