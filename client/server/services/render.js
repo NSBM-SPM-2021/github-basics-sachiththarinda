78c6052cc3165cdf061625ef416fbbc633fa006d
sachiththarinda <sachith.tharinda@gmail.com>
2021-08-24 17:05:41 +0530

    using API

--- client/server/services/render.js
+++ client/server/services/render.js
@@ -1,7 +1,16 @@
+const axios = require('axios');
 
 
 exports.homeRoutes =(req,res)=>{
-    res.render('index');
+    axios.get('http://localhost:3000/api/patients')
+    .then(function(response){
+        console.log(response.data)
+        res.render('index', { patients : response.data });
+    })
+    .catch(err =>{
+        res.send(err);
+    })
+   
 }
 
 exports.add_patient =(req,res)=>{
